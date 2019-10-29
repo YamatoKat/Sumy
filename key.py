600d35cddba1bbfd3f1442b9f3f33a006438c9cb
Yamato <katsuya850@gmail.com>
2019-10-29 14:32:03 +0900

    import packeges

--- key.py
+++ key.py
@@ -1,2 +1,18 @@
 # coding: utf-8
+import spacy
+from urllib import request
+from bs4 import BeautifulSoup
+import bs4
+
+
+nlp = spacy.load('ja_ginza')
+
+#doc = nlp('これがライブラリの実験です')
+#for sent in doc.sents:
+#    for token in sent:
+#        print(token.i, token.orth_, token.lemma_, token.pos_, token.tag_, token.dep_, token.head.i)
+#   print('EOS')
+
+
+
 
